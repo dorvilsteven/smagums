b0be47e8b517e18a8cbd0a397dabe3118a16217d
Lady Gaga <jessiezheqi.li@gmail.com>
2021-11-13 17:46:43 -0800

    Add some dummy data

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO merchandise (product_name, product_type, brand_name, img, price)
+VALUES
+  ('Wide Fit oxford shoes', 'shoes', 'ASOS DESIGN', 'images.asos-media.com/products/asos-design-wide-fit-oxford-shoes-in-tan-leather-with-toe-cap/12315538-1-tan', 51),
+  ('backless mule loafer', 'shoes', 'ASOS DESIGN', 'images.asos-media.com/products/asos-design-backless-mule-loafer-in-black-leather/12011431-1-black', 54)
