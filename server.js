908a32e0c93a5dee50d5bf564964dd96c38a93dc
Lady Gaga <jessiezheqi.li@gmail.com>
2021-11-15 18:38:19 -0800

    Add names of routes planned

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ app.get("/", (req, res) => {
   res.send("hi");
 });
 
+// Helper functions:
+function searchProducts() {
+  if (query.brandName) {
+    // brandName
+  }
+  // productTypes
+}
+
+// API routes:
+// router.get(‘/api/products/:id’, callback function)
+// router.get(‘/api/products’, callback function) {
+//     searchProducts()
+// }
+// router.get(‘/api/favorites’, callback)
+
+// router.get(‘/login’, callback)
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
