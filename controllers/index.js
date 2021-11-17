60661ca637f154acf4f1541f6767a3418b8a866a
StevenD <dorvilsteven@gmail.com>
2021-11-17 14:16:30 -0500

    added basic routes and basic layout to login and signup

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const homeRoutes = require('./home-routes.js');
 const shopRoutes = require('./shop-routes.js');
+const loginRoutes = require('./login-routes.js');
+const signUpRoutes = require('./signup-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/shop', shopRoutes);
+router.use('/login', loginRoutes);
+router.use('/signup', signUpRoutes);
 
 module.exports = router;
