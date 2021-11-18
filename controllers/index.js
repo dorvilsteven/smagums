ac331cf12fb6830372c7d47fe351d61a19fed188
Lady Gaga <jessiezheqi.li@gmail.com>
2021-11-18 11:26:21 -0800

    Add packaging for /api

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-const homeRoutes = require('./home-routes.js');
-const shopRoutes = require('./shop-routes.js');
+router.use("/api", apiRoutes);
 
-router.use('/', homeRoutes);
-router.use('/shop', shopRoutes);
+router.use((req, res) => {
+  // when the request is made to a not-defined endpoint
+  res.status(404).end();
+});
+
+// const homeRoutes = require("./home-routes.js");
+// const shopRoutes = require("./shop-routes.js");
+
+// router.use("/", homeRoutes);
+// router.use("/shop", shopRoutes);
 
 module.exports = router;
