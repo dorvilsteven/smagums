253d4dc944a70245fd1465b6075d19cdd0bf02b8
Lady Gaga <jessiezheqi.li@gmail.com>
2021-11-18 15:55:16 -0800

    Reverse previous commented code with catch error

--- controllers/index.js
+++ controllers/index.js
@@ -3,15 +3,15 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
+const homeRoutes = require("./home-routes");
+const shopRoutes = require("./shop-routes");
+
+router.use("/", homeRoutes);
+router.use("/shop", shopRoutes);
+
 router.use((req, res) => {
   // when the request is made to a not-defined endpoint
   res.status(404).end();
 });
-
-const homeRoutes = require("./home-routes.js");
-const shopRoutes = require("./shop-routes.js");
-
-router.use("/", homeRoutes);
-router.use("/shop", shopRoutes);
 
 module.exports = router;
