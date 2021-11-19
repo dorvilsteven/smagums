1d23b5676a07862454d3656ab77c4283dbc764ec
Lady Gaga <jessiezheqi.li@gmail.com>
2021-11-19 13:35:02 -0800

    Render more info for one product

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
-const productEl = document.getElementById("single-product");
-const getProductBtn = document.querySelector("#get-product");
+const productEl = $("#single-product");
+const getProductBtn = $("#get-product");
 
 const formSubmitHandler = function (event) {
   event.preventDefault();
@@ -26,7 +26,7 @@ const formSubmitHandler = function (event) {
   //   body: JSON.stringify(productObject),
   // }).then();
 
-  fetch("/api/products/5", {
+  fetch("/api/products/6", {
     method: "GET",
   }).then((response) => {
     if (response.ok) {
@@ -35,19 +35,31 @@ const formSubmitHandler = function (event) {
 
         //for one product
         // clear old content every time search is performed
-        productEl.textContent = "";
+        productEl.text('');
 
-        const containerEl = document.createElement("div");
-        containerEl.classList =
-          "list-item flex-row justify-space-between align-center";
+        const containerEl = $("<p></p>");
+        containerEl.addClass(
+          "list-item flex-row justify-space-between align-center"
+        );
 
-        const productName = data.product_name;
-        const titleEl = document.createElement("span");
-        titleEl.textContent = productName;
+        // const productName = data.product_name;
+        // const productType = ;
+        // const brandName = ;
+        // const priceTag = ;
+        const titleEl = $("<span></span>");
+        titleEl.text(`${data.product_name}
+          ${data.product_type}
+          ${data.brand_name} 
+          ${data.price}`);
 
-        containerEl.appendChild(titleEl);
-        productEl.appendChild(containerEl);
-
+        // showImage
+        const imageEl = $("<img></img>");
+        imageEl.attr('src', `https://${data.product_image_url}`)
+        imageEl.appendTo(titleEl)
+        
+        titleEl.appendTo(containerEl);
+        containerEl.appendTo(productEl);
+        
         // for all matching products
         // for (let i = 0; i < data.length; i++) {
         //   // (middle element) create a container for each repo
@@ -57,7 +69,7 @@ const formSubmitHandler = function (event) {
     }
   });
 };
-getProductBtn.addEventListener("click", formSubmitHandler); // use 'click' instead of 'submit' to prevent requirement of JSON Formatting
+getProductBtn.on("click", formSubmitHandler);
 
 // function showAll() {}
 // showAll();
