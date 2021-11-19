93c8c82066ff0d66519f473535d699daa17b70f2
Lady Gaga <jessiezheqi.li@gmail.com>
2021-11-18 19:51:01 -0800

    Refactor

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
 
 // middleware
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // Add routes
 app.use(routes);
