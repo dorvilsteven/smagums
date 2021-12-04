e1d72f628557ae636d5c165bede0e07af4462a53
Lady Gaga <jessiezheqi.li@gmail.com>
2021-12-04 13:48:04 -0800

    Replace shop page input checkboxes w radio buttons

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,72 +1,94 @@
-// gender 
-const men = $('#men');
-const women = $('#women');
-// product type 
-const shirts = $('#shirts');
-const jackets = $('#jackets');
-const bottoms = $('#bottoms');
-const shoes = $('#shoes');
-// brand 
-const asos = $('#ASOS');
-const vans = $('#Vans');
-const nike = $('#Nike');
-const lacoste = $('#Lacoste');
+// gender
+const men = $("#men");
+const women = $("#women");
+// product type
+const shirts = $("#shirts");
+const jackets = $("#jackets");
+const bottoms = $("#bottoms");
+const shoes = $("#shoes");
+// brand
+const asos = $("#ASOS");
+const vans = $("#Vans");
+const nike = $("#Nike");
+const lacoste = $("#Lacoste");
 // price
-const sale = $('#49');
-const regular = $('#99');
-const expensive = $('#100');
+const sale = $("#49");
+const regular = $("#99");
+const expensive = $("#100");
 // form
-const filterForm = $('#filter-form');
-const formBtn = $('#form-submit');
+const filterForm = $("#filter-form");
+const formBtn = $("#form-submit");
 // products display
 const productEl = $("#products");
 
 // line break
-const lineBreak = '<br>';
+const lineBreak = "<br>";
 
-const options = [men, women, shirts, jackets, bottoms, shoes, asos, vans, nike, lacoste, sale, regular, expensive];
+const options = [
+  men,
+  women,
+  shirts,
+  jackets,
+  bottoms,
+  shoes,
+  asos,
+  vans,
+  nike,
+  lacoste,
+  sale,
+  regular,
+  expensive,
+];
 
-const isChecked = (filter) => {
-    if (filter[0].checked) {
-        return true;
-    } else {
-        return false;
-    }
-}
+const isSelected = (filter) => {
+  if (filter[0].checked) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 const showProduct = (data) => {
-    // get image, name, brand, product type and price 
-    const productName = $('<h3>').addClass('card-title').text(data.product_name), 
-          productType = $('<p>').addClass('card-text').text(data.product_type), 
-          productBrand = $('<h3>').addClass('card-title').text(data.brand_name), 
-          productPrice = $('<p>').addClass('card-text').text(data.price)
-          productImg = $('<img>').attr('src', `https://${data.product_image_url}`),
-          productID = data.id;
+  // get image, name, brand, product type and price
+  const productName = $("<h3>").addClass("card-title").text(data.product_name),
+    productType = $("<p>").addClass("card-text").text(data.product_type),
+    productBrand = $("<h3>").addClass("card-title").text(data.brand_name),
+    productPrice = $("<p>").addClass("card-text").text(data.price);
+  (productImg = $("<img>").attr("src", `https://${data.product_image_url}`)),
+    (productID = data.id);
 
-    const imgContainer = $('<div>').addClass('img-container').append(productImg);
+  const imgContainer = $("<div>").addClass("img-container").append(productImg);
 
-    
-    // button 
-    const button = $('<a>').addClass('btn btn-secondary mx-auto product-button').attr('href', `/${productID}`).text('View Product');
+  // button
+  const button = $("<a>")
+    .addClass("btn btn-secondary mx-auto product-button")
+    .attr("href", `/${productID}`)
+    .text("View Product");
 
-    const cardBody =  $('<div>').addClass('card-body w-100').append(
-        productName,
-        productType,
-        lineBreak,
-        productBrand,
-        productPrice,
-        lineBreak,
-        button
+  const cardBody = $("<div>")
+    .addClass("card-body w-100")
+    .append(
+      productName,
+      productType,
+      lineBreak,
+      productBrand,
+      productPrice,
+      lineBreak,
+      button
     );
-    // create card element
-    const card = $('<div>').addClass('card m-3').append(
-        $('<div>').addClass('row').append(
-            $('<div>').addClass('col-xs-12 col-sm-6').append(imgContainer),
-            $('<div>').addClass('col-xs-12 col-sm-6').append(cardBody)
+  // create card element
+  const card = $("<div>")
+    .addClass("card m-3")
+    .append(
+      $("<div>")
+        .addClass("row")
+        .append(
+          $("<div>").addClass("col-xs-12 col-sm-6").append(imgContainer),
+          $("<div>").addClass("col-xs-12 col-sm-6").append(cardBody)
         )
     );
 
-    productEl.append(card);
+  productEl.append(card);
 };
 
 // const formSubmitHandler = (filterList) => {
@@ -87,28 +109,29 @@ const showProduct = (data) => {
 //     // })
 // };
 
-formBtn.on('click', (event) => {
-    event.preventDefault();
-    const finalFilter = [];
-    options.forEach((filter) => {
-        if (isChecked(filter)) {
-            finalFilter.push(filter[0].id);
-        }
-    });
-    console.log(finalFilter);
-
+formBtn.on("click", (event) => {
+  event.preventDefault();
+  const finalFilter = [];
+  options.forEach((filter) => {
+    if (isSelected(filter)) {
+      finalFilter.push(filter[0].id);
+    }
+  });
+  //console.log(finalFilter);
 });
 
 $(document).ready(() => {
-    fetch('/api/products', {
-        method: "GET",
-    }).then((response) => {
-        if (response.ok) {
-            return response.json();
-        }
-    }).then((data) => {
-        console.log(data);
-        productEl.text('');
-        data.forEach((product) => showProduct(product));
+  fetch("/api/products", {
+    method: "GET",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      productEl.text("");
+      data.forEach((product) => showProduct(product));
     });
 });
